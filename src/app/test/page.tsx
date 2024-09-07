41ccc660ea8578a6cea683fcab425ae0ab1376fd
kwangsun <eyeyel@naver.com>
2024-09-08 00:48:41 +0900

    test: 불필요 코드 제거

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,13 +3,13 @@
 import Scraping from "@/components/scraping";
 import { Suspense } from "react";
 import SkeletonLoading from "./(common)/SkeletonLoading";
-import { Target, targetToKorean } from "@/utils/scraping";
+import { ScrapTarget, targetToKorean } from "@/utils/types";
 import classNames from "classnames/bind";
 import styles from "./page.module.scss";
 
 const cn = classNames.bind(styles);
 
-const TARGETS: Target[] = ["HANA_TOUR", "MODE_TOUR", "ONLINE_TOUR"];
+const TARGETS: ScrapTarget[] = ["HANA_TOUR", "MODE_TOUR", "ONLINE_TOUR"];
 
 function Test() {
   return (
