8b4581bef668b050ba60570320dd1a38e4f16192
kwangsun <eyeyel@naver.com>
2024-09-21 22:56:40 +0900

    feat: 모두투어 지역 스크래핑 api 추가

---
+++ src/utils/api.ts
@@ -0,0 +1,34 @@
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+import { isDev } from "./constant";
+
+export function getScrapResponse(type: "ERROR" | "OK", result?: unknown) {
+  if (type === "OK") {
+    return new Response(result ? JSON.stringify({ result }) : null, {
+      status: 200,
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  return new Response(null, {
+    status: 500,
+    headers: { "content-type": "application/json" },
+  });
+}
+
+export async function getBrowser() {
+  chromium.setGraphicsMode = false;
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    // defaultViewport: chromium.defaultViewport,
+    executablePath: isDev
+      ? await puppeteer.executablePath("chrome")
+      : await chromium.executablePath(),
+    // headless: chromium.headless,
+    defaultViewport: { width: 500, height: 500 },
+    headless: false,
+  });
+
+  return browser;
+}
